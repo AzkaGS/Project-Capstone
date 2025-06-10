c1b6e10af55d41fa7246db41f9ea338a2392e9e3
AZKAAA <127210925+AzkaGS@users.noreply.github.com>
2025-06-10 22:10:51 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.write("Hello world!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+
+from imblearn.over_sampling import SMOTE
+
+from sklearn.model_selection import GridSearchCV
+
+# Membaca dataset
+df = pd.read_csv('/content/ObesityDataSet.csv')
+print(df.info())
+
+print("Beberapa baris pertama:")
+print(df.head())
